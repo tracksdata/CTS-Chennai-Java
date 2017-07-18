5466d0a11c2eaae4246de2e5d7485cfb796a80fd
richardsforu <praveen.somireddy@gmail.com>
2017-07-18 13:18:49 +0530

    XML added

---
+++ XML/Test/src/com/Test.java
@@ -0,0 +1,16 @@
+package com;
+
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Date d1 = new Date();
+		Date d2 = new Date();
+		Date d3 = new Date();
+		Date d4 = new Date();
+
+	}
+
+}
---
+++ XML/Test/src/edu/Use.java
@@ -0,0 +1,14 @@
+package edu;
+
+import com.Test;
+
+public class Use {
+
+	public static void main(String[] args) {
+		
+		Test t=new Test();
+		
+		
+	}
+
+}
